4be4135ba973e091e5aabefff4960040b134a0f4
w1nteRR <mykola796@gmail.com>
2021-03-29 00:00:37 +0300

    add auth flow for navigation

--- src/navigation/root.tsx
+++ src/navigation/root.tsx
@@ -1,17 +1,46 @@
 import React, { FC } from 'react'
+import { Text, View } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 
-
 import { BottomTabBar } from './bars/bottom.bar'
+import { Auth } from '../screens/auth/Auth'
+
+import { login } from '../redux/auth/thunks'
+import { useAppDispatch, useAppSelector } from '../hooks/useRedux'
 
 const Stack = createStackNavigator()
 
 export const RootStackScreen: FC = () => {
+
+  const { checking, isAuthenticated } = useAppSelector(state => state.auth)
+  const dispatch = useAppDispatch()
+
+  if(checking) {
+    dispatch(login())
+
+    return <View style={{ flex: 1}}>
+      <Text>Checking auth</Text>
+    </View>
+  }
+
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name='root' component={BottomTabBar} />
+        {
+          isAuthenticated
+          ? <Stack.Screen 
+              name='root' 
+              component={BottomTabBar} 
+            />
+          : <Stack.Screen 
+              name='signin' 
+              component={Auth} 
+              options={{
+                headerShown: false
+              }}
+            />
+        }
       </Stack.Navigator>
     </NavigationContainer>
   )
