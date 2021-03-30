db2151c70a3b5c293c2d093d9128a23bb1429395
w1nteRR <mykola796@gmail.com>
2021-03-31 00:01:01 +0300

    update app

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
 import React from 'react'
 import { Provider } from 'react-redux'
+import { useColorScheme } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native'
 
 import { RootStackScreen } from './navigation/root'
 import store from './redux/store'
+import { darkTheme, lightTheme } from './theme'
 
 export const App = () => {
+
+  const scheme = useColorScheme()
+
   return (
-    <Provider store={store}>
-      <RootStackScreen />
-    </Provider>
+    <NavigationContainer theme={scheme === 'dark' ? darkTheme : lightTheme}>
+      <Provider store={store}>
+        <RootStackScreen />
+      </Provider>
+    </NavigationContainer>
   )
 }
