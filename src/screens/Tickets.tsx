59595fb8dcf767dbb23b742a437bf5b34d0ca8d7
w1nteRR <mykola796@gmail.com>
2021-04-01 22:55:13 +0300

    add basic screens

---
+++ src/screens/Tickets.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from "react"
+import { Text, View } from "react-native"
+
+export const Tickets: FC = () => {
+  return (
+    <View>
+      <Text>
+        Tickets screen
+      </Text>
+    </View>
+  )
+}
---
+++ src/screens/Watchlist.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from "react"
+import { Text, View } from "react-native"
+
+export const Watchlist: FC = () => {
+  return (
+    <View>
+      <Text>
+        Watchlist screen
+      </Text>
+    </View>
+  )
+}
