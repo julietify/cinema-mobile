522c3a63bcf8d82d09868c56fc1bd875fdea9340
w1nteRR <mykola796@gmail.com>
2021-04-03 22:43:48 +0300

    update bottom bar

--- src/navigation/bars/bottom.bar.config.tsx
+++ src/navigation/bars/bottom.bar.config.tsx
@@ -1,31 +1,33 @@
 import React from 'react'
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
 
+import { palette } from '../../theme/palette'
+
 export const renderBarIcons = (value: string, focused: boolean) => {
   switch (value) {
     case 'Home':
       return (
         focused
-          ? <Icon name='home' size={25} color='black' />
-          : <Icon name='home' size={25} color='orange' />
+          ? <Icon name='home' size={25} color={palette.theme.dark} />
+          : <Icon name='home-outline' size={25} color='gray' />
       )
     case 'Tickets':
       return (
         focused
-          ? <Icon name='ticket-confirmation' size={25} color='black' />
-          : <Icon name='ticket-confirmation' size={25} color='orange' />
+          ? <Icon name='ticket-confirmation' size={25} color={palette.theme.dark} />
+          : <Icon name='ticket-confirmation-outline' size={25} color='gray' />
       )
     case 'Watchlist':
       return (
         focused
-          ? <Icon name='heart' size={25} color='black' />
-          : <Icon name='heart' size={25} color='orange' />
+          ? <Icon name='heart' size={25} color={palette.theme.dark} />
+          : <Icon name='heart-outline' size={25} color='gray' />
       )
     case 'Calendar':
       return (
         focused
-          ? <Icon name='calendar' size={25} color='black' />
-          : <Icon name='calendar' size={25} color='orange' />
+          ? <Icon name='calendar' size={25} color={palette.theme.dark} />
+          : <Icon name='calendar-outline' size={25} color='gray' />
       )
   }
 }
