bc731e4982f562e316465a2175a8e29998c0c4c6
R4msey <taras200230@gmail.com>
2021-04-06 15:39:34 +0300

    add props

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -3,10 +3,15 @@ import { Text, View, ScrollView } from "react-native"
 import { CardFilm } from "../components/common/card.film"
 import { TestComponent } from "../components/common/text.component"
 export const Home: FC = () => {  
+
+  const picture  = 'https://karavan.ua/wp-content/uploads/2019/12/74682950_738377696672108_7910429065208903771_n.jpg'
+  const name  = 'No time to die'
+  const genre  = 'Action / Mistery  -  2 hours'
+
   return (
     <ScrollView>
       <TestComponent/>
-        <CardFilm/>
+        <CardFilm picture={picture} name={name} genre={genre}/>
     </ScrollView>
   )
 }
