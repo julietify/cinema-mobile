f489b7d9b6bcf372206a1e706dd6f4cc399aaeb4
w1nteRR <mykola796@gmail.com>
2021-04-09 00:01:56 +0300

    update icon button

--- src/components/ui/buttons/button.icon.tsx
+++ src/components/ui/buttons/button.icon.tsx
@@ -1,5 +1,5 @@
 import React, { FC } from 'react'
-import { Pressable, Text, View, StyleSheet, useWindowDimensions } from 'react-native'
+import { Pressable, Text, View, StyleSheet, useWindowDimensions, TouchableOpacity } from 'react-native'
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
 
 import { IButton } from '../../../interfaces/IButton'
@@ -23,8 +23,9 @@ export const ButtonIcon: FC<IButtonIcon> = ({
   const defaultWidth = window.width - 50
 
   return (
-    <Pressable 
+    <TouchableOpacity 
       onPress={onPress} 
+      activeOpacity={.9}
     >
       <View 
         style={{ 
@@ -36,7 +37,7 @@ export const ButtonIcon: FC<IButtonIcon> = ({
         <Icon name={iconName} size={25} color={iconColor || '#fff'} />
         { text && <Text style={styles.text}>{text}</Text> }
       </View>
-    </Pressable>
+    </TouchableOpacity>
   )
 }
 
