d6b4b2e9e96c59132adb84b42fc65980f379b57d
R4msey <taras200230@gmail.com>
2021-04-08 21:13:25 +0300

    add ticket.error

--- src/screens/Tickets.tsx
+++ src/screens/Tickets.tsx
@@ -1,12 +1,26 @@
-import React, { FC } from "react"
-import { Text, View } from "react-native"
+import React, { FC, useState } from "react"
+import { Text, View, StyleSheet } from "react-native"
+
+import { TicketsError } from '../components/errors/tickets.error'
 
 export const Tickets: FC = () => {
+
+  const [tickets, setTickets] = useState([])
+
+  if(!tickets.length) return <TicketsError />
+
   return (
-    <View>
-      <Text>
-        Tickets screen
-      </Text>
+    <View style={styles.container}>
+      <Text>screen page</Text>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    justifyContent:'center',
+    flex: 1
+  }
+})
+
