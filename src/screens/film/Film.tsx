de59dda7bb160053ae7a114f46518b3f5b5d794b
w1nteRR <mykola796@gmail.com>
2021-04-14 01:38:15 +0300

    rework film screen

--- src/screens/film/Film.tsx
+++ src/screens/film/Film.tsx
@@ -1,9 +1,20 @@
-import React, { FC } from "react"
-import { View, Text, ScrollView, StyleSheet } from "react-native"
+import React, { FC, useState } from "react"
+import { View, StyleSheet, ScrollView } from "react-native"
 
-import { FilmContainer } from '../../components/film/film.container'
+import { FilmCard } from "../../components/common/film.card"
+import { Info } from "../../components/film/views/info.view"
+
+import { Wrapper } from "../../components/film/wrappers/film.wrapper"
+import { ViewWrapper } from "../../components/film/wrappers/view.wrapper"
+
+import { ButtonBack } from "../../components/ui/buttons/button.back"
+import { ButtonIcon } from "../../components/ui/buttons/button.icon"
+import { Title, Body2 } from "../../components/ui/typography/title"
 
 import type { FilmRouteProp } from '../../navigation/stacks/film.stack'
+
+import { palette } from "../../theme/palette"
+import { films } from '../../utils/films'
 
 interface IFilmProps {
   route: FilmRouteProp
@@ -13,21 +24,82 @@ export const Film: FC<IFilmProps> = ({
   route
 }) => {
 
-  const { params } = route
+  const { filmId } = route.params
+
+  const [film, setState] = useState(() => {
+    return films.filter(film => film.id === filmId)[0]
+  })
+
+  console.log(film)
+
   return (
+    <>
     <View style={styles.container}>
-      <FilmContainer 
-        id={params.filmId} 
-        picture={params.filmPicture} 
-        name={params.filmName} 
-        time={params.filmTime} 
-        genre={params.filmGenre}
-        story={params.filmStory} />
+      <Wrapper uri={film.poster}>
+        <ScrollView>
+          <ButtonBack />
+          <FilmCard 
+            picture={film.poster} 
+            onPress={() => {}} 
+          />
+          <View style={styles.nameContainer}>
+            <Title>{film.name}</Title>
+          </View>
+          <View style={styles.trailerContainer}>
+            <ButtonIcon 
+              iconName='play'
+              text='Watch trailer'
+              onPress={() => {}} 
+              {...trailerButton}
+            />
+            <Info time={film.time} genre={film.genre} />
+          </View>
+          <ViewWrapper title='Storyline'>
+            <Body2>{film.story}</Body2>
+          </ViewWrapper>
+        </ScrollView>
+        <View style={styles.bookContainer}>
+          <ButtonIcon 
+            text='Book now' 
+            iconName='ticket-confirmation'
+            onPress={() => {}} 
+            {...bookButton}
+          />
+        </View>
+      </Wrapper>
     </View>
+    </>
   )
 }
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 1
   },
+  nameContainer: {
+    alignItems: 'center',
+    marginVertical: 20
+  },
+  trailerContainer: {
+    alignItems: 'center'
+  },
+  bookContainer:{
+    bottom: 0, 
+    position: 'relative', 
+    width: '100%', 
+    alignItems: 'center', 
+    backgroundColor: 'transparent'
+  }
 })
+
+const trailerButton = {
+  brRadius: 30,
+  p: 10,
+  w: 160,
+  bgColor: palette.main.silver
+}
+
+const bookButton = {
+  brRadius: 15,
+  p: 13,
+  bgColor: palette.main.lightblue 
+}
