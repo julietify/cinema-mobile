e33bd7a518a1c4e10d8809f8d15338e3c4e4560a
R4msey <taras200230@gmail.com>
2021-04-13 22:18:10 +0300

    add films

--- src/utils/films.ts
+++ src/utils/films.ts
@@ -2,26 +2,49 @@ export const films = [
   {
     id: '1',
     name: 'No Time To Die',
-    poster: 'https://karavan.ua/wp-content/uploads/2019/12/74682950_738377696672108_7910429065208903771_n.jpg'
+    poster: 'https://karavan.ua/wp-content/uploads/2019/12/74682950_738377696672108_7910429065208903771_n.jpg',
+    genre: 'Action | Adventure',
+    time: '92min',
+    story:'Bond has left active service and is enjoying a tranquil life in Jamaica. His peace is short-lived when his old friend Felix Leiter from the CIA turns up asking for help. The mission to rescue a kidnapped scientist turns out to be far more treacherous than expected, leading Bond onto the trail of a mysterious villain armed with dangerous new technology.'
   },
   {
     id: '2',
     name: 'Below Zero',
-    poster: 'https://media2.firstshowing.net/firstshowing/img11/BelowZeroSpanishposterbigimg5991.jpg'
+    poster: 'https://m.media-amazon.com/images/M/MV5BMjg0NTBjOWEtMDc1OS00ZDEyLWE3ZTItMGQzYTIyZDJkM2U2XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg',
+    genre: 'Crime | Drama',
+    time: '121min',
+    story:'On a lonely road, a prison transport is brutally assaulted. Martin, the policeman who was driving, survives and fortifies his position while the con men search for a way to finish him.'
   },
   {
     id: '3',
     name: 'Tom & Jerry',
-    poster: 'https://media.kg-portal.ru/movies/t/tomandjerry/posters/tomandjerry_3.jpg'
+    poster: 'https://media.kg-portal.ru/movies/t/tomandjerry/posters/tomandjerry_3.jpg',
+    genre: 'Animation | Comedy',
+    time: '84min',
+    story:'A chaotic battle ensues between Jerry Mouse, who has taken refuge in the Royal Gate Hotel, and Tom Cat, who is hired to drive him away before the day of a big wedding arrives.'
   },
   {
     id: '4',
     name: 'The Dig',
-    poster: 'https://kritikanstvo.ru/movies/t/thedig/posters/thedig_300858.jpg'
+    poster: 'https://kritikanstvo.ru/movies/t/thedig/posters/thedig_300858.jpg',
+    genre: 'Action | Adventure',
+    time: '172min',
+    story:'An archaeologist embarks on the historically important excavation of Sutton Hoo in 1938'
   },
   {
     id: '5',
     name: 'Bliss',
-    poster: 'https://i2.wp.com/media-news.com.ua/wp-content/uploads/2021/01/Bliss-poster.jpg?ssl=1'
+    poster: 'https://i2.wp.com/media-news.com.ua/wp-content/uploads/2021/01/Bliss-poster.jpg?ssl=1',
+    genre: 'Action | Adventure',
+    time: '132min',
+    story:'Bliss is a mind-bending love story following Greg (Owen Wilson) who, after recently being divorced and then fired, meets the mysterious Isabel (Salma Hayek), a woman living on the streets and convinced that the polluted, broken world around them is just a computer simulation. Doubtful at first, Greg eventually discovers there may be some truth to Isabels wild conspiracy.'
+  },
+  {
+    id: '6',
+    name: 'The Rise of Skywalker',
+    poster: 'https://resizing.flixster.com/nyQEEUdokTK91hsyvk18xi7tGTE=/ems.ZW1zLXByZC1hc3NldHMvbW92aWVzL2RhMGM0OTk2LTRiZDktNDg1ZC05NzQ5LTUzMzQwZjA1OWFlMy53ZWJw',
+    genre: 'Fantasy | Sci-Fi',
+    time: '148min',
+    story:'While the First Order continues to ravage the galaxy, Rey finalizes her training as a Jedi. But danger suddenly rises from the ashes as the evil Emperor Palpatine mysteriously returns from the dead. While working with Finn and Poe Dameron to fulfill a new mission, Rey will not only face Kylo Ren once more, but she will also finally discover the truth about her parents as well as a deadly secret that could determine her future and the fate of the ultimate final showdown that is to come. '
   }
 ]
