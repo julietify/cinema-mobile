63436958db95c261250154b6716274b4e1446176
w1nteRR <mykola796@gmail.com>
2021-04-17 16:12:22 +0300

    add rating view

---
+++ src/components/film/views/rating.view.tsx
@@ -0,0 +1,51 @@
+import React, { memo } from 'react'
+import { View, StyleSheet } from 'react-native'
+
+import { palette } from '../../../theme/palette'
+import { Title, Body1 } from "../../ui/typography/title"
+
+import { IOmdbRating } from '../../../interfaces/IOmdb'
+interface IFilmRating{
+  imdbRating: string
+  rating: Array<IOmdbRating>
+}
+
+export const Rating = memo<IFilmRating>(({
+  rating
+}) => {
+
+  if (!rating.length) return null
+
+  const newRating = rating.map((item, index) => index === 0 ? {...item, Source: 'IMDb'} : item)
+
+  const _ratingItem = (source: string, value: string, key: number) => {
+    return (
+      <View key={key} style={styles.ratingItemContainer}>
+        <Title fontSize='13px'>{value}</Title>
+        <Body1>{source}</Body1>
+      </View>
+    )
+  }
+
+  return(
+    <View style={styles.container}>
+      { newRating.map((i, index) => _ratingItem(i.Source, i.Value, index)) }
+    </View>
+  )
+
+})
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    width: '90%',
+    justifyContent: 'space-around',
+    marginVertical: 20,
+    padding: 20,
+    backgroundColor: palette.main.silver,
+    borderRadius: 20
+  },
+  ratingItemContainer: {
+    alignItems: 'center'
+  }
+})
