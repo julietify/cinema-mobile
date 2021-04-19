138d5e5801a32294d49c2988934acbd997376be2
w1nteRR <mykola796@gmail.com>
2021-04-20 00:25:14 +0300

    add tickets screen

---
+++ src/screens/ticket/Tickets.tsx
@@ -0,0 +1,40 @@
+import React, { FC } from "react"
+import {View, FlatList, StyleSheet } from "react-native"
+
+import { TicketCard } from "../../components/ticket/cards/ticket.card" 
+import { useRedirect } from "../../hooks/useRedirect"
+
+import { tickets } from '../../utils/tickets'
+
+export const Tickets: FC = () => {
+
+  const { redirectoToTicketScreen } = useRedirect()
+
+  //const [tickets, setTickets] = useState([])
+  //if(!tickets.length) return <TicketsError />
+
+  return (
+    <View style={styles.wrapper}>
+      <FlatList 
+        data={tickets}
+        renderItem={({ item, index }) => 
+          <TicketCard 
+            name={item.name} 
+            cinema={item.cinema} 
+            tickets={item.tickets.length} 
+            price={item.price} 
+            ticketId={index.toString()}
+            onPress={redirectoToTicketScreen}
+          />
+        }
+        keyExtractor={(_, index) => index.toString()}
+      />
+    </View>
+  )
+}
+const styles = StyleSheet.create({
+  wrapper: {
+    flex: 1,
+    alignItems: 'center'
+  }
+})
