cd36e283eb8c3fead1e32251f81d763c464fb947
R4msey <taras200230@gmail.com>
2021-04-19 21:21:32 +0300

    add ticket card

---
+++ src/utils/tickets.tsx
@@ -0,0 +1,24 @@
+export const tickets = [
+  {
+    order: '12311233',
+    name: 'No time to die',
+    cinema:'Multiplex',
+    tickets: [{sited:'F4', code:'6870847817'},{sited:'F5', code:'8937498173'},{sited:'F9', code:'6870127817'},{sited:'F6', code:'8237498173'}],
+    date:'17 February',
+    startTime:'17:45',
+    price:'45',
+    technology:'Cinetech+',
+    age:'12+',
+  },
+  {
+    order: '21231243',
+    name: 'Star Wars: Episode IX - The Rise of Skywalker',
+    cinema:'Multiplex',
+    tickets: [{sited:'F4', code:'6870847867'}],
+    date:'17 February',
+    startTime:'16:45',
+    price:'45',
+    technology:'Cinetech+',
+    age:'12+',
+  }
+]
