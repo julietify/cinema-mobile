ab0bc5b81e7ba747b3cf1636928e2f80a830b365
w1nteRR <mykola796@gmail.com>
2021-04-20 00:25:30 +0300

    update bottom bar

--- src/navigation/bars/bottom.bar.tsx
+++ src/navigation/bars/bottom.bar.tsx
@@ -6,6 +6,8 @@ import { Tickets } from '../../screens/ticket/Tickets'
 import { Watchlist } from '../../screens/Watchlist'
 import { Calendar } from '../../screens/Calendar'
 
+import { TicketStackScreen } from '../stacks/ticket.stack'
+
 import { renderBarIcons, options } from './bottom.bar.config'
 
 const Tab = createBottomTabNavigator()
@@ -20,7 +22,7 @@ export const BottomTabBar: FC = () => {
       tabBarOptions={options}
     >
       <Tab.Screen name='Home' component={Home} />
-      <Tab.Screen name='Tickets' component={Tickets} />
+      <Tab.Screen name='Tickets' component={TicketStackScreen} />
       <Tab.Screen name='Watchlist' component={Watchlist} />
       <Tab.Screen name='Calendar' component={Calendar} />
     </Tab.Navigator>
