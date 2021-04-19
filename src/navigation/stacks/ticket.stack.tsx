71b21b78fe6f269f06c440ff7d76c082e90c005f
w1nteRR <mykola796@gmail.com>
2021-04-20 00:24:57 +0300

    rework ticket nav stack

--- src/navigation/stacks/ticket.stack.tsx
+++ src/navigation/stacks/ticket.stack.tsx
@@ -1,21 +1,30 @@
 import React from 'react'
-import { createStackNavigator } from '@react-navigation/stack'
+import { CardStyleInterpolators, createStackNavigator } from '@react-navigation/stack'
 import { RouteProp } from '@react-navigation/native'
 
-import { TicketContainer } from '../../components/ticket/ticket.container'
+import { Tickets } from '../../screens/ticket/Tickets'
+import { Ticket } from '../../screens/ticket/Ticket'
+
+import { screenConfig } from '../config/screen.config'
 
 type TicketStackParams = {
-  RootTicket: {
-    ticketOrder: string
+  RootTicket: undefined
+  Ticket: {
+    ticketId: string
   }
 }
 
 const TicketStack = createStackNavigator<TicketStackParams>()
 
-export type TicketRouteProp = RouteProp<TicketStackParams, 'RootTicket'>
+export type TicketRouteProp = RouteProp<TicketStackParams, 'Ticket'>
 
 export const TicketStackScreen = () => (
-  <TicketStack.Navigator initialRouteName='RootTicket' headerMode='none'>
-    <TicketStack.Screen name="RootTicket" component={TicketContainer} />
+  <TicketStack.Navigator initialRouteName='RootTicket' headerMode='none' 
+    screenOptions={{
+      cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS
+    }}
+  >
+    <TicketStack.Screen name="RootTicket" component={Tickets} options={screenConfig.ticket} />
+    <TicketStack.Screen name="Ticket" component={Ticket}  />
   </TicketStack.Navigator>
 )
