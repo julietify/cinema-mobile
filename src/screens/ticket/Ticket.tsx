e5da28105598dc1127f52f2997c04514ead2d500
w1nteRR <mykola796@gmail.com>
2021-04-22 20:35:49 +0300

    refactor

--- src/screens/ticket/Ticket.tsx
+++ src/screens/ticket/Ticket.tsx
@@ -1,14 +1,13 @@
-import React, { FC, useState } from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import React, { FC, useCallback, useState } from 'react'
+import { View, StyleSheet, FlatList } from 'react-native'
 
-import type { TicketRouteProp } from '../../navigation/stacks/ticket.stack'
-
-import { tickets } from '../../utils/tickets'
-import { TicketContainer } from '../../components/ticket/ticket.container'
+import { TicketItem } from '../../components/ticket/ticket/ticket.item'
 import { ClientInformation } from '../../components/ticket/cards/client.information'
 
-import { ButtonBack } from '../../components/ui/buttons/button.back'
-import { FlatList } from 'react-native-gesture-handler'
+import { Title1 } from '../../components/ui/typography/title'
+
+import { tickets } from '../../utils/tickets'
+import type { TicketRouteProp } from '../../navigation/stacks/ticket.stack'
 
 interface ITicketProps {
   route: TicketRouteProp
@@ -18,19 +17,25 @@ export const Ticket: FC<ITicketProps> = ({
 }) => {
 
   const { ticketId } = route.params
-
-  /*const iHeight = iWidth / 15
-  const lineHeight = iWidth / 80*/
+  const [currentTicket, setCurrentTicket] = useState(null)
 
   const [ticket] = useState(() => tickets.filter(ticket => ticket.order === ticketId)[0])
 
+  const _onViewableItemsChanged = useCallback(({ viewableItems }) => {
+    // console.log('viewable item', viewableItems)
+    setCurrentTicket(viewableItems[0].index + 1)
+  }, [])
+
   return (
     <View style={styles.wrapper}>
+      <View style={styles.counterContainer}>
+        <Title1>{currentTicket} of {ticket.tickets.length} Tickets</Title1>
+      </View>
       <View style={styles.ticketContainer}>
         <FlatList
           data={ticket.tickets}
           renderItem={({ item }) =>
-            <TicketContainer
+            <TicketItem
               key={item.seat}
               name={ticket.name}
               code={item.code}
@@ -44,37 +49,32 @@ export const Ticket: FC<ITicketProps> = ({
           }
           keyExtractor={(_, index) => index.toString()}
           horizontal
+          pagingEnabled
           decelerationRate={0.99}
           showsHorizontalScrollIndicator={false}
+          onViewableItemsChanged={_onViewableItemsChanged}
+          viewabilityConfig={{
+            itemVisiblePercentThreshold: 50
+          }}
         />
       </View>
       <ClientInformation
         client={ticket.client}
         price={ticket.price}
-        />
+      />
     </View>
   )
 }
 const styles = StyleSheet.create({
   wrapper: {
-    flex: 1,
-  },
-  buttonsContainer: {
-    flexDirection: 'row',
-    paddingHorizontal: 20,
-    alignItems: 'center',
-    height: 120
+    flex: 1
   },
   ticketContainer: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center'
+  },
+  counterContainer: {
+    margin: 20
   }
 })
-
-
-/*
-<View style={styles.buttonsContainer}>
-          <ButtonBack />
-        </View>
- */
