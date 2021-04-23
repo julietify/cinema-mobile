4169cc59d9a6a69f01edd25f8cb787f5bb361d8d
w1nteRR <mykola796@gmail.com>
2021-04-23 23:23:54 +0300

    update heading

--- src/components/common/heading.tsx
+++ src/components/common/heading.tsx
@@ -1,19 +1,30 @@
-import React, { memo } from "react"
-import { useWindowDimensions, View } from "react-native"
+import React, { memo, ReactNode } from "react"
+import { StyleSheet, useWindowDimensions, View } from "react-native"
 import { useTheme } from "@react-navigation/native"
 
 import { Title } from "../ui/typography/title"
 
-export const Heading = memo<{ value: string }>(({
-  value
+export const Heading = memo<{ value: string, children?: ReactNode }>(({
+  value,
+  children
 }) => {
 
   const { colors } = useTheme()
   const { width } = useWindowDimensions()
 
   return (
-    <View style={{ width }}>
-      <Title color={colors.text} style={{ padding: 20 }}>{value}</Title>
+    <View style={{ ...styles.container, width }}>
+      <Title color={colors.text}>{value}</Title>
+      {children}
     </View>
   )
+})
+
+const styles = StyleSheet.create({
+    container: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'space-between',
+      padding: 20
+    }
 })
