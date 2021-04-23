125e577f3801836b3da10e9e4c0ae7747eb193ea
R4msey <taras200230@gmail.com>
2021-04-23 20:43:13 +0300

    add trailers api

--- src/api/tmdb.api.ts
+++ src/api/tmdb.api.ts
@@ -11,6 +11,9 @@ export const tmdbApi = () => {
   return {
     cast: async (filmId: string) => {
       return (await api.get(`/movie/${filmId}/credits?api_key=${key}`)).data.cast
+    },
+    trailers: async (filmId: string) => {
+      return (await api.get(`/movie/${filmId}/videos?api_key=${key}`)).data.results
     }
   }
 }
