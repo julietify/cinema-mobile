50307818319f4d9f03c37c86e834cdb88f169d91
R4msey <taras200230@gmail.com>
2021-04-30 21:01:15 +0300

    rework flatlist

--- src/screens/Watchlist.tsx
+++ src/screens/Watchlist.tsx
@@ -1,14 +1,39 @@
 import React, { FC } from "react"
-import { Text, View } from "react-native"
+import { View, StyleSheet, FlatList } from "react-native"
 
 import { Heading } from "../components/common/heading"
-import { WatchlistCard } from "../components/watchlist/watchlist.card"
+import { Card } from "../components/common/card"
+
+import { useRedirect } from "../hooks/useRedirect"
+
+import { liked } from '../utils/liked'
 
 export const Watchlist: FC = () => {
+
+  const { redirectToFilmScreen } = useRedirect()
+  
   return (
-    <View>
+    <View style={styles.wrapper}>
       <Heading value='Watchlist' />
-      <WatchlistCard  filmId='SS' name='ss' picture='s' />
+      <FlatList 
+        data={liked}
+        renderItem={({ item }) => 
+          <Card
+            id={item.filmId}
+            onPress={redirectToFilmScreen}
+            lines={[item.name,item.genre,item.date]}
+            poster={item.poster}
+          />
+        }
+        keyExtractor={(_, index) => index.toString()}
+      />
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  wrapper: {
+    flex:1,
+    alignItems:'center'
+  } 
+})
