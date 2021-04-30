f601cbe0156fe0977a7f99c647e0cda83216c5d2
R4msey <taras200230@gmail.com>
2021-04-30 21:02:19 +0300

    add liked card

---
+++ src/utils/liked.ts
@@ -0,0 +1,16 @@
+export const liked = [
+  {
+    filmId:'tt2382320',
+    name: 'No time to die',
+    date: '17 September',
+    genre:'Action, Adventure, Thriller',
+    poster: 'https://karavan.ua/wp-content/uploads/2019/12/74682950_738377696672108_7910429065208903771_n.jpg'
+  },
+  {
+    filmId:'tt2527338',
+    name: 'Star Wars: Episode IX - The Rise of Skywalker',
+    genre:'Action, Adventure, Fantasy',
+    date: '17 September',
+    poster: 'https://resizing.flixster.com/nyQEEUdokTK91hsyvk18xi7tGTE=/ems.ZW1zLXByZC1hc3NldHMvbW92aWVzL2RhMGM0OTk2LTRiZDktNDg1ZC05NzQ5LTUzMzQwZjA1OWFlMy53ZWJw'
+  }
+]
